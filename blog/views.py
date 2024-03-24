c3f35bdef1cd8a5397937ff8dd7e6479077be85d
Dark-Data-Science <j7416284@gmail.com>
2024-03-24 15:30:52 -0300

    modicada

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,9 @@ from .models import Post
 def renderPosts(request):
     total_posts = Post.objects.count()
     posts = Post.objects.order_by("-date")
-    return render(request, "blog.html", {"posts": posts, "total_posts": total_posts})
+    return render(request, "blog/blog.html", {"posts": posts, "total_posts": total_posts})
 
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, "post_detail.html", {"post": post})
+    return render(request, "blog/post_detail.html", {"post": post})
